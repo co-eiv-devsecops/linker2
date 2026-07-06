d12d16a1d2539b88bd26e820393f4c08029ede44
JuanDiegoRV <jd.rvelasquezp@gmail.com>
2026-07-05 19:46:54 -0500

    feat(flags): support LaunchDarkly feature flags

--- feature_flags.py
+++ feature_flags.py
@@ -1,12 +1,30 @@
 import os
 
+try:
+    import ldclient
+    from ldclient import Context
+    from ldclient.config import Config
+except ImportError:
+    ldclient = None
+    Context = None
+    Config = None
+
 
 FLAG_ENV_VARS = {
     "custom_alias": "LINKER_ENABLE_CUSTOM_ALIAS",
 }
 
+LAUNCHDARKLY_FLAG_KEYS = {
+    "custom_alias": "custom-alias",
+}
+
+LAUNCHDARKLY_SDK_KEY_ENV = "LAUNCHDARKLY_SDK_KEY"
+LAUNCHDARKLY_CONTEXT_KEY_ENV = "LAUNCHDARKLY_CONTEXT_KEY"
+DEFAULT_CONTEXT_KEY = "linker-python"
 TRUE_VALUES = {"1", "true", "yes", "on", "enabled"}
 FALSE_VALUES = {"0", "false", "no", "off", "disabled"}
+_launchdarkly_client = None
+_launchdarkly_sdk_key = None
 
 
 def parse_bool(value, default=False):
@@ -21,8 +39,46 @@ def parse_bool(value, default=False):
     return default
 
 
-def is_enabled(flag_name, environ=os.environ):
+def local_flag_value(flag_name, environ=os.environ):
     env_var = FLAG_ENV_VARS.get(flag_name)
     if env_var is None:
         return False
     return parse_bool(environ.get(env_var), default=False)
+
+
+def launchdarkly_context(environ=os.environ):
+    context_key = environ.get(LAUNCHDARKLY_CONTEXT_KEY_ENV, DEFAULT_CONTEXT_KEY)
+    if Context is None:
+        return context_key
+    return Context.builder(context_key).name("Linker Python").build()
+
+
+def get_launchdarkly_client(environ=os.environ):
+    global _launchdarkly_client, _launchdarkly_sdk_key
+
+    sdk_key = environ.get(LAUNCHDARKLY_SDK_KEY_ENV)
+    if not sdk_key or ldclient is None or Config is None:
+        return None
+
+    if _launchdarkly_client is None or _launchdarkly_sdk_key != sdk_key:
+        ldclient.set_config(Config(sdk_key))
+        _launchdarkly_client = ldclient.get()
+        _launchdarkly_sdk_key = sdk_key
+
+    return _launchdarkly_client
+
+
+def is_enabled(flag_name, environ=os.environ, launchdarkly_client=None):
+    default = local_flag_value(flag_name, environ)
+    flag_key = LAUNCHDARKLY_FLAG_KEYS.get(flag_name)
+    if flag_key is None:
+        return False
+
+    client = launchdarkly_client or get_launchdarkly_client(environ)
+    if client is None:
+        return default
+
+    try:
+        return bool(client.variation(flag_key, launchdarkly_context(environ), default))
+    except Exception:
+        return default
--- tests/test_feature_flags.py
+++ tests/test_feature_flags.py
@@ -1,6 +1,19 @@
 import unittest
 
-from feature_flags import is_enabled, parse_bool
+from feature_flags import is_enabled, local_flag_value, parse_bool
+
+
+class FakeLaunchDarklyClient:
+    def __init__(self, value=None, error=None):
+        self.value = value
+        self.error = error
+        self.calls = []
+
+    def variation(self, flag_key, context, default):
+        self.calls.append((flag_key, context, default))
+        if self.error:
+            raise self.error
+        return self.value
 
 
 class FeatureFlagsTest(unittest.TestCase):
@@ -25,6 +38,23 @@ class FeatureFlagsTest(unittest.TestCase):
 
     def test_is_enabled_returns_false_for_unknown_flags(self):
         self.assertFalse(is_enabled("unknown", {"UNKNOWN": "true"}))
+
+    def test_local_flag_value_reads_environment_fallback(self):
+        environ = {"LINKER_ENABLE_CUSTOM_ALIAS": "true"}
+
+        self.assertTrue(local_flag_value("custom_alias", environ))
+
+    def test_is_enabled_reads_launchdarkly_variation_when_client_is_available(self):
+        client = FakeLaunchDarklyClient(value=True)
+
+        self.assertTrue(is_enabled("custom_alias", {}, launchdarkly_client=client))
+        self.assertEqual(client.calls[0][0], "custom-alias")
+
+    def test_is_enabled_uses_local_default_when_launchdarkly_fails(self):
+        client = FakeLaunchDarklyClient(error=RuntimeError("LaunchDarkly unavailable"))
+        environ = {"LINKER_ENABLE_CUSTOM_ALIAS": "true"}
+
+        self.assertTrue(is_enabled("custom_alias", environ, launchdarkly_client=client))
 
 
 if __name__ == "__main__":
